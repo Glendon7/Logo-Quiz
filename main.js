c109bedbcb28d211ca950d6141899d91c05dd717
Glendon7 <53873888+Glendon7@users.noreply.github.com>
2020-06-05 23:10:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,333 @@
+function check(op){
+
+var question;
+
+switch (op) {
+  case 1:
+
+  question = document.quiz.question1.value.toLowerCase();
+
+  if (question == "webclues infotech")
+  {document.quiz.question1.style.background = '#0be881';
+}
+  else {
+    document.quiz.question1.style.background = '#ff5252';
+
+  }
+  break;
+
+  case 2:
+
+   question = document.quiz.question2.value.toLowerCase();
+
+  if (question == "magneto it solutions")
+  {document.quiz.question2.style.background = '#0be881';
+}
+  else {
+    document.quiz.question2.style.background = '#ff5252';
+
+  }
+break;
+
+case 3:
+
+  question = document.quiz.question3.value.toLowerCase();
+  if (question == "mphasis")
+  {document.quiz.question3.style.background = '#0be881';
+}
+  else {
+    document.quiz.question3.style.background = '#ff5252';
+
+  }
+break;
+
+case 4:
+
+    question = document.quiz.question4.value.toLowerCase();
+
+  if (question == "appentus")
+  {document.quiz.question4.style.background = '#0be881';
+  }
+  else {
+    document.quiz.question4.style.background = '#ff5252';
+
+  }
+break;
+
+case 5:
+
+    question = document.quiz.question5.value.toLowerCase();
+
+  if (question == "trigent")
+  {document.quiz.question5.style.background = '#0be881';
+  }
+  else {
+    document.quiz.question5.style.background = '#ff5252';
+  }
+break;
+
+case 6:
+
+    question = document.quiz.question6.value.toLowerCase();
+
+    if (question == "endive software")
+    {document.quiz.question6.style.background = '#0be881';
+  }
+    else {
+      document.quiz.question6.style.background = '#ff5252';
+
+    }
+
+break;
+
+case 7:
+
+     question = document.quiz.question7.value.toLowerCase();
+
+      if (question == "confianz global")
+      {document.quiz.question7.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question7.style.background = '#ff5252';
+
+      }
+break;
+
+case 8:
+
+     question = document.quiz.question8.value.toLowerCase();
+
+      if (question == "wildnet technologies")
+      {document.quiz.question8.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question8.style.background = '#ff5252';
+
+      }
+break;
+
+case 9:
+
+     question = document.quiz.question9.value.toLowerCase();
+
+      if (question == "tata consultancy services")
+      {document.quiz.question9.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question9.style.background = '#ff5252';
+
+      }
+break;
+
+case 10:
+
+     question = document.quiz.question10.value.toLowerCase();
+
+      if (question == "infosys")
+      {document.quiz.question10.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question10.style.background = '#ff5252';
+
+      }
+break;
+
+
+case 11:
+
+     question = document.quiz.question11.value.toLowerCase();
+
+      if (question == "clarion technologies")
+      {document.quiz.question11.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question11.style.background = '#ff5252';
+
+      }
+break;
+
+case 12:
+
+     question = document.quiz.question12.value.toLowerCase();
+
+      if (question == "wipro")
+      {document.quiz.question12.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question12.style.background = '#ff5252';
+
+      }
+break;
+
+case 13:
+
+     question = document.quiz.question13.value.toLowerCase();
+
+      if (question == "ideas2it")
+      {document.quiz.question13.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question13.style.background = '#ff5252';
+
+      }
+break;
+
+case 14:
+
+     question = document.quiz.question14.value.toLowerCase();
+
+      if (question == "hcl technologies")
+      {document.quiz.question14.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question14.style.background = '#ff5252';
+
+      }
+break;
+
+case 15:
+
+     question = document.quiz.question15.value.toLowerCase();
+
+      if (question == "larsen & toubro infotech")
+      {document.quiz.question15.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question15.style.background = '#ff5252';
+
+      }
+break;
+
+case 16:
+
+     question = document.quiz.question16.value.toLowerCase();
+
+      if (question == "tech mahindra")
+      {document.quiz.question16.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question16.style.background = '#ff5252';
+
+      }
+break;
+
+case 17:
+
+     question = document.quiz.question17.value.toLowerCase();
+
+      if (question == "techjini")
+      {document.quiz.question17.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question17.style.background = '#ff5252';
+
+      }
+break;
+
+case 18:
+
+     question = document.quiz.question18.value.toLowerCase();
+
+      if (question == "mindtree")
+      {document.quiz.question18.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question18.style.background = '#ff5252';
+
+      }
+break;
+
+case 19:
+
+     question = document.quiz.question19.value.toLowerCase();
+
+      if (question == "sasken")
+      {document.quiz.question19.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question19.style.background = '#ff5252';
+
+      }
+break;
+
+case 20:
+
+     question = document.quiz.question20.value.toLowerCase();
+
+      if (question == "oracle financial services")
+      {document.quiz.question20.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question20.style.background = '#ff5252';
+
+      }
+break;
+
+case 21:
+
+     question = document.quiz.question21.value.toLowerCase();
+
+      if (question == "rolta")
+      {document.quiz.question21.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question21.style.background = '#ff5252';
+
+      }
+break;
+
+case 22:
+
+     question = document.quiz.question22.value.toLowerCase();
+
+      if (question == "cybage")
+      {document.quiz.question22.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question22.style.background = '#ff5252';
+
+      }
+break;
+
+case 23:
+
+     question = document.quiz.question23.value.toLowerCase();
+
+      if (question == "blue star")
+      {document.quiz.question23.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question23.style.background = '#ff5252';
+
+      }
+break;
+
+case 24:
+
+     question = document.quiz.question24.value.toLowerCase();
+
+      if (question == "aftek")
+      {document.quiz.question24.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question24.style.background = '#ff5252';
+
+      }
+break;
+
+case 25:
+
+     question = document.quiz.question25.value.toLowerCase();
+
+      if (question == "3i infotech")
+      {document.quiz.question25.style.background = '#0be881';
+    }
+      else {
+        document.quiz.question25.style.background = '#ff5252';
+
+      }
+break;
+
+
+}
+}
